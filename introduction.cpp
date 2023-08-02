fa00821c0f89d8565e2ee6373033a8913e32b49d
Lakshay soni <lakshaysoni5252@gmail.com>
2023-08-02 23:52:49 +0530

    first commit

---
+++ introduction.cpp
@@ -0,0 +1,32 @@
+// #include <iostream.h>
+// using namespace std;
+// int main()
+// {
+//     cout << "hello this is c++";
+// }
+
+// #include<iostream>
+// // using namespace std;
+// int main()
+// {
+//     int x,y;
+//     cout<<"enter the data"<<endl;
+//     cin>>x>>y;
+//     cout<<"sum is="<<x+y;
+// }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y,z;
+    cout<<"enter the data"<<endl;
+    cin>>x>>y>>z;
+    cout<<""
+
+}
+
+//program to find largest number among three numbers 
+// to calculate sum of natural numbers 
+// to check wether numbers is prime or not 
+// to display fibonacci series 
